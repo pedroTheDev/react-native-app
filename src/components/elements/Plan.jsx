52999bf197e29e090092340ee63668c4253609c9
Pedro Duarte <ikbenpedro@gmail.com>
2021-03-02 17:30:32 -0300

    obs text

--- src/components/elements/Plan.jsx
+++ src/components/elements/Plan.jsx
@@ -31,14 +31,22 @@ export default function Plan() {
         <Text style={styles.titleText}>{element.name}</Text>
         <View style={styles.content}>
           <Text style={styles.itens}>Pós Pago - À vista</Text>
-          <Text style={styles.itens}>{element.phonePrice}</Text>
+          <Text style={styles.itensNumber}>{`R$${element.phonePrice.toFixed(2)}`}</Text>
           <Text style={styles.itens}>Mensalidade total do Plano</Text>
-          <Text style={styles.itens}>{element.monthly_fee}</Text>
-          <Text style={styles.itens}>Pre Pago* - À vista</Text>
-          <Text style={styles.itens}>{element.phonePriceOnPlan}</Text>
+          <Text style={styles.itensNumber}>{`R$${element.monthly_fee.toFixed(2)}`}</Text>
+          <Text style={styles.itens}>Pré Pago* - À vista</Text>
+          <Text style={styles.itensNumber}>{`R$${element.phonePriceOnPlan.toFixed(2)}`}</Text>
         </View>
       </View>
       )}
+      <Text>
+        Oferta sujeita a permanência mínima de 12 meses. Consulte
+        regulamento e periodo de fidelização. Parcelamento em até
+        12 vezes sem juros, válido para pagamento com cartão de
+        crédito. Chip (R$12,00) não incluso no valor do aparelho.
+        Oferta sujeita à análise de crédito.
+        *Valor do aparelho avulso.
+      </Text>
     </View>
   )
 }
@@ -46,12 +54,13 @@ export default function Plan() {
 const styles = StyleSheet.create({
   container:{
     backgroundColor:'#dedede',
+    borderRadius: 5,
     margin:20,
   },
   titleText:{
     margin:15,
     height: '1rem',
-    fontSize:18,
+    fontSize:20,
     fontWeight:'bold',
   },
   content:{
@@ -60,11 +69,26 @@ const styles = StyleSheet.create({
     backgroundColor:'#fff',
     margin:10,
     padding:0,
+    alignItems: 'center',
   },
   itens:{
     border: '1px solid #dedede',
     margin:0,
     fontSize:15,
+    height:40,
+    alignItems: 'center',
+    paddingTop:8,
+    paddingLeft: 8,
+  },
+  itensNumber:{
+    border: '1px solid #dedede',
+    margin:0,
+    fontSize:15,
+    height:40,
+    textAlign: 'center',
+    color:'#5396ed',
+    paddingTop:8,
+    
   },
 
 })
